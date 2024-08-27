5990f90d86f48c6296b35966f64677e113503d5a
SeaNot-Not <seancortez41@gmail.com>
2024-08-27 10:01:08 +0800

    TodoItems Custom Hook Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,83 @@
-import { useEffect, useState } from "react"
-import "./styles.css"
-import { NewTodoForm } from "./NewTodoForm"
-import { TodoList } from "./TodoList"
-import { FilterOptions } from "./FilterOptions"
+import { useEffect, useState } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+import { FilterOptions } from "./FilterOptions";
+import { useTodoItems } from "./useTodoItems.js";
 
 export default function App() {
-//---------- Declaration of Hooks -----------
-const [todoItems, setTodoItems] = useState(() => {
-  const savedItems = localStorage.getItem("ITEMS")
-  return savedItems == null ? [] : JSON.parse(savedItems)
-})
+  //Custom Hook for Todo Items State
+  const [todoItems, setTodoItems, addTodoItem, toggleTodoItem, deleteItem] =
+    useTodoItems();
 
-//---------------For Sorting Options----------------
-const [filterOption, setfilterOption] = useState('none')
-const [filteredItems, setFilteredItems] = useState([])
+  //---------------For Sorting Options----------------
+  const [filterOption, setfilterOption] = useState("none");
+  const [filteredItems, setFilteredItems] = useState([]);
 
-//----------------For Search Feature--------------------
-const [searchQuery, setSearchQuery] = useState("")
+  //----------------For Search Feature--------------------
+  const [searchQuery, setSearchQuery] = useState("");
 
-//----------------For Search and Filter Feature------------------
+  //----------------For Search and Filter Feature------------------
 
-useEffect(() => {
-  filterItems(filterOption, todoItems, searchQuery)
-}, [filterOption, todoItems, searchQuery])
+  useEffect(() => {
+    filterItems(filterOption, todoItems, searchQuery);
+  }, [filterOption, todoItems, searchQuery]);
 
-//-------------For Saving to Local Storage--------------------
-useEffect(() => {
-  localStorage.setItem("ITEMS", JSON.stringify(todoItems))
-}, [todoItems])
+  const handleRadioChange = (e) => {
+    const newFilterOption = e.target.value;
+    setfilterOption(newFilterOption);
+  };
 
-//--------- Functions -----------
+  const filterItems = (newFilterOption, todoItems, searchQuery) => {
+    let filtered;
 
-const addTodoItem = (title) => { //Add Todo Item
-   setTodoItems(todos => {
-    return [...todos, {id: crypto.randomUUID(), title, completed: false},]
-   })
-}
+    if (newFilterOption === "completed") {
+      filtered = [...todoItems].filter((item) => item.completed === true);
+    } else if (newFilterOption === "pending") {
+      filtered = [...todoItems].filter((item) => item.completed === false);
+    } else {
+      filtered = [...todoItems];
+    }
 
-const toggleTodoItem = (id, completed) => { //Toggle Todo Item
-  return setTodoItems(todos => {
-    return todos.map(todo => todo.id === id ? {...todo, completed} : todo)
-  })
-}
+    if (searchQuery !== "") {
+      filtered = filtered.filter((item) =>
+        item.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
 
-const deleteItem = (id) => { // Delete Item Function
-  return setTodoItems(todos => {
-    return todos.filter(todo => id !== todo.id)
-  })
-}
+    setFilteredItems(filtered);
+  };
 
-const handleRadioChange = (e) => {
-  const newFilterOption = e.target.value
-  setfilterOption(newFilterOption)
-}
+  const handleSearchQuery = (value) => {
+    setSearchQuery(value);
+  };
 
-const filterItems = (newFilterOption, todoItems, searchQuery) => {
-  let filtered
+  const searchClear = () => {
+    setSearchQuery("");
+  };
 
-  if (newFilterOption === "completed") {
-    filtered = [...todoItems].filter(item => item.completed === true)
-  } else if (newFilterOption === "pending") {
-    filtered = [...todoItems].filter(item => item.completed === false)
-  } else {
-    filtered = [...todoItems]
-  }
-
-  if(searchQuery !== "") {
-    filtered = filtered.filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()))
-  }
-
-  setFilteredItems(filtered)  
-}
-
-const handleSearchQuery = (value) => {
-  setSearchQuery(value)
-}
-
-const searchClear = () => {
-  setSearchQuery("")
-}
-
-//--------- What you see on screen -----------
+  //--------- What you see on screen -----------
   return (
     <>
-    <div className="card-box">
-      <NewTodoForm addTodoItem={addTodoItem} />
-      <FilterOptions 
-        searchQuery={searchQuery} 
-        handleSearchQuery={handleSearchQuery} 
-        filterOption={filterOption} 
-        handleRadioChange={handleRadioChange}
-        searchClear={searchClear}
-      />
-      
-      <h2 className="header">Todo List</h2>
+      <div className="card-box">
+        <NewTodoForm addTodoItem={addTodoItem} />
+        <FilterOptions
+          searchQuery={searchQuery}
+          handleSearchQuery={handleSearchQuery}
+          filterOption={filterOption}
+          handleRadioChange={handleRadioChange}
+          searchClear={searchClear}
+        />
 
-      <TodoList 
-        todoItems={filteredItems}
-        toggleTodoItem={toggleTodoItem}
-        deleteItem={deleteItem}
-        setTodoItems={setTodoItems}
-      />
-    </div>
+        <h2 className="header">Todo List</h2>
+
+        <TodoList
+          todoItems={filteredItems}
+          toggleTodoItem={toggleTodoItem}
+          deleteItem={deleteItem}
+          setTodoItems={setTodoItems}
+        />
+      </div>
     </>
-  )
+  );
 }
