8235c0702f2554486e62371bb625b159586dcfb7
SeaNot-Not <seancortez41@gmail.com>
2024-08-27 10:48:52 +0800

    Custom Hook Practice

--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -1,59 +1,86 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function TodoItems({ id, title, completed, toggleTodoItem, deleteItem, setTodoItems }) {
-    const [isEditing, setIsEditing] = useState(false)
-    const [itemEditing, setItemEditing] = useState("")
-    const [editedTodo, setEditedTodo] = useState("")
+export function TodoItems({
+  id,
+  title,
+  completed,
+  toggleTodoItem,
+  deleteItem,
+  setTodoItems,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [itemEditing, setItemEditing] = useState("");
+  const [editedTodo, setEditedTodo] = useState("");
 
-    const handleEditButton = (id) => {
-        setIsEditing(true)
-        setItemEditing(id)
+  const handleEditButton = (id) => {
+    setIsEditing(true);
+    setItemEditing(id);
+  };
+
+  const handleResetButton = () => {
+    setIsEditing(false);
+    setEditedTodo("");
+    setItemEditing("");
+  };
+
+  const handleSaveButton = (itemEditing, newTodo) => {
+    if (newTodo === "") {
+      alert("Please input a string!");
+    } else {
+      setTodoItems((todos) => {
+        return todos.map((todo) => {
+          if (itemEditing === todo.id) {
+            return { ...todo, title: newTodo };
+          } else {
+            return todo;
+          }
+        });
+      });
+      setIsEditing(false);
+      setEditedTodo("");
+      setItemEditing("");
     }
+  };
 
-    const handleResetButton = () => {
-        setIsEditing(false)
-        setEditedTodo("")
-        setItemEditing("")
-    }
-
-    const handleSaveButton = (itemEditing, newTodo) => {
-        if (newTodo === null) {
-            alert("Please input a string!")
-        } else {
-            setTodoItems(todos => {
-                return todos.map(todo => {
-                    if (itemEditing === todo.id) {
-                        return { ...todo, title: newTodo }
-                    } else {
-                        return todo
-                    }
-                })
-            })
-            setIsEditing(false)
-            setEditedTodo("")
-            setItemEditing("")
-        }
-    }
-
-    return (
-        <li>
-              <label>
-                <input className="todo-checkbox" type="checkbox" checked={completed} onChange={(e) => toggleTodoItem(id, e.target.checked)} />
-                {title}
-              </label>
-              {itemEditing === id && isEditing ? (
-                <>
-                  <button onClick={() => handleSaveButton(id, editedTodo)} className="btn btn-edit">✔</button>
-                  <button className="btn btn-danger" onClick={() => handleResetButton} >✖</button>
-                  <input value={editedTodo} onChange={e => setEditedTodo(e.target.value)} className="text-input" type="text" />
-                </>
-                ) : (
-                  <>
-                    <button onClick={() => handleEditButton(id)} className="btn btn-edit">Edit</button>
-                    <button className="btn btn-danger" onClick={() => deleteItem(id)}>Delete</button>
-                  </>
-                ) 
-              }
-        </li>
-    )
+  return (
+    <li>
+      <label>
+        <input
+          className="todo-checkbox"
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodoItem(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      {itemEditing === id && isEditing ? (
+        <>
+          <button
+            onClick={() => handleSaveButton(id, editedTodo)}
+            className="btn btn-edit"
+          >
+            ✔
+          </button>
+          <button className="btn btn-danger" onClick={() => handleResetButton}>
+            ✖
+          </button>
+          <input
+            value={editedTodo}
+            onChange={(e) => setEditedTodo(e.target.value)}
+            className="text-input"
+            type="text"
+          />
+        </>
+      ) : (
+        <>
+          <button onClick={() => handleEditButton(id)} className="btn btn-edit">
+            Edit
+          </button>
+          <button className="btn btn-danger" onClick={() => deleteItem(id)}>
+            Delete
+          </button>
+        </>
+      )}
+    </li>
+  );
 }
